2d2a023caf37e408f28d120e7de5138598774771
Neddymond <Neddymond10@gmail.com>
2018-10-02 19:53:52 -0700

    Added Password to stages in the game.

---
+++ Assets/Hacker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ShowMainMenu ();
+	}
+
+	Terminal.WriteLine
+	void ShowMainMenu () {
+		Terminal.ClearScreen ();
+		var greeting = "Hello Ben";
+		Terminal.WriteLine (greeting);
+		Terminal.WriteLine ("What would you like to hack into?");
+		Terminal.WriteLine ("Press 1 for Mouwa's Library");
+		Terminal.WriteLine ("Press 2 for Wole Soyinka's Library?");
+		Terminal.WriteLine ("Press 3 for Chinedu's Library?");
+		Terminal.WriteLine ("Enter your Selection:");
+ }
+
+	void OnUserInput (string Input) {
+	print(Input);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
